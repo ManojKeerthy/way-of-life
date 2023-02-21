b4a54327a09a99000ffbf8c356960cb07bec8375
Manoj Kumar Keerthy <manojkumarkeerthy@gmail.com>
2023-02-21 22:43:52 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ConnectioHabits from "./components/pages/ConnectionHabits"
+import HomePage from "./components/pages/HomePage"
+import FinancialHabits from "./components/pages/FinancialHabits"
+import HealthyHabits from "./components/pages/HealthyHabits"
+import MindsetHabits from "./components/pages/MindsetHabits"
+import ProductivityHabits from "./components/pages/ProductivityHabits"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar/>
+      <Routes>
+        <Route path="/"  element={<HomePage/>} />
+        <Route path="/connection" element={<ConnectioHabits/>} />
+        <Route path="/finance"  element={<FinancialHabits/>} />
+        <Route path="/healthy"  element={<HealthyHabits/>} />
+        <Route path="/mindset"  element={<MindsetHabits/>} />
+        <Route path="/productivity"  element={<ProductivityHabits/>} />
+      </Routes>
+    </Router>
   );
 }
 
